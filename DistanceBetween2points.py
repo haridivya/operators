8ad1b860387cd4240a6c424fa104ae4956b1724a
haridivya <123063502+haridivya@users.noreply.github.com>
2024-09-25 19:28:14 +0530

    Update DistanceBetween2points.py

--- DistanceBetween2points.py
+++ DistanceBetween2points.py
@@ -22,5 +22,5 @@ y1 = int(input())
 x2 = int(input())
 y2 = int(input())
 distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-print(f"The distance between Ajay's house and Chandru's house is {distance:.2f}")
+print("The distance between Ajay's house and Chandru's house is ""%.2f"%distance)
 
